dc19bc6c19103020a1177841c92c40f71d036e8b
Shiva2520 <www.shivangairan@gmail.com>
2024-08-20 01:58:30 +0530

    updated main function solved defer cancel problem and also added some part of delete todo function

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/thedevsaddam/renderer"
-	"golang.org/x/text/message"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -50,70 +50,88 @@ func init() {
 	db = sess.DB(dbName)
 }
 
-func homeHandler(w http.ResponseWriter,r *http.Request){
-	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"},nil)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"}, nil)
 	checkErr(err)
 }
 
-func fetchTodos(w http.ResponseWriter, r *http.Request){
+func fetchTodos(w http.ResponseWriter, r *http.Request) {
 	todos := []todoModel{}
 
-	if err := db.C(collectionName).Find(bson.M{}).All(&todos); err != nil{
+	if err := db.C(collectionName).Find(bson.M{}).All(&todos); err != nil {
 		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message":"Failed to fetch todo",
-			"error":err,
+			"message": "Failed to fetch todo",
+			"error":   err,
 		})
 		return
 	}
 	todolist := []todo{}
 
-	for _, t := range todos{
+	for _, t := range todos {
 		todolist = append(todolist, todo{
-			ID: t.ID.Hex(),
-			Title: t.Title,
+			ID:        t.ID.Hex(),
+			Title:     t.Title,
 			Completed: t.Completed,
 			CreatedAt: t.CreatedAt,
 		})
 	}
-	rnd.JSON(w,http.StatusOK,renderer.M{
-		"data":todolist,
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data": todolist,
 	})
 }
 
-func createTodo(w http.ResponseWriter,r *http.Request){
+func createTodo(w http.ResponseWriter, r *http.Request) {
 	var t todo
 
-	if err := json.NewDecoder(r.Body).Decode(&t) ; err != nil{//whatever body was inside r will be copied inside t
-		rnd.JSON(w,http.StatusProcessing,err)
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil { //whatever body was inside r will be copied inside t
+		rnd.JSON(w, http.StatusProcessing, err)
 		return
 	}
 
-	if t.Title == ""{
-		rnd.JSON(w,http.StatusProcessing,renderer.M{
-			"message":"The title is required",
+	if t.Title == "" {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "The title is required",
 		})
 		return
 	}
 
 	tm := todoModel{
-		ID: bson.NewObjectId(),
-		Title: t.Title,
+		ID:        bson.NewObjectId(),
+		Title:     t.Title,
 		Completed: false,
 		CreatedAt: time.Now(),
 	}
 
-	if err := db.C(collectionName).Insert(&tm); err != nil{
-		rnd.JSON(w,http.StatusProcessing,renderer.M{
-			"message":"Failed to save todo",
-			"error":err,
+	if err := db.C(collectionName).Insert(&tm); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to save todo",
+			"error":   err,
 		})
 		return
 	}
 
-	rnd.JSON(w,http.StatusCreated,renderer.M{
-		"message":"Created Successfully",
-		"todo_id":tm.ID.Hex(),
+	rnd.JSON(w, http.StatusCreated, renderer.M{
+		"message": "Created Successfully",
+		"todo_id": tm.ID.Hex(),
 	})
+}
+
+func deletetodo(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id) {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "The id is invalid",
+		})
+		return
+	}
+
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to delete",
+			"error":   err,
+		})
+	}
 }
 
 func main() {
@@ -142,9 +160,8 @@ func main() {
 	log.Println("shutting down server...")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	srv.Shutdown(ctx)
-	defer cancel(
-		log.Println("server gracefully stopped!")
-	)
+	defer cancel()
+	log.Println("server gracefully stopped!")
 }
 
 func todoHandlers() http.Handler {
@@ -153,7 +170,7 @@ func todoHandlers() http.Handler {
 		r.Get("/", fetchTodos)
 		r.Post("/", createTodo)
 		r.Put("/{id}", updateTodo)
-		r.Delete("/{id}", dzeleteTodo)
+		r.Delete("/{id}", deleteTodo)
 	})
 	return rg
 }
